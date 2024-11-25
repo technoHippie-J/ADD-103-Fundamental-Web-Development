98a44d1a8e0c70bea6c26e0d1734177dfea31fb7
technoHippie-J <tjanus@students.mchenry.edu>
2024-11-24 19:10:16 -0600

    portfolio - web page complete

---
+++ projects/portfolio/js/script.js
@@ -0,0 +1,14 @@
+/* change the image */
+function updateIframes(htmlUrl, cssUrl, renderedUrl, clickedLink) {
+    // update the iframes
+    document.getElementById('iframe-html').src = htmlUrl;
+    document.getElementById('iframe-css').src = cssUrl;
+    document.getElementById('iframe-site').src = renderedUrl;
+
+    // remove active class from previous link
+    const links = document.querySelectorAll('.assign-link');
+    links.forEach(link => link.classList.remove('active'));
+
+    // add active class to the clicked link
+    clickedLink.classList.add('active');
+}
